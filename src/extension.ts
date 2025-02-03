bc6882366af95dd22e7fbefffb046e276a90c20d
Jauamzin <juanjnow@gmail.com>
2025-02-03 19:09:30 -0300

    First layout test

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "css-easyfix" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('css-easyfix.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from css easyfix!');
-	});
-
-	context.subscriptions.push(disposable);
+  let disposable = vscode.commands.registerCommand('css-easyfix.helloWorld', () => {
+    const panel = vscode.window.createWebviewPanel(
+      'cssEasyFix',           // Internal identifier for the webview.
+      'CSS EasyFix',          // Panel title.
+      vscode.ViewColumn.Beside, // Where to show the panel.
+      { enableScripts: true } // Webview options.
+    );
+    
+    panel.webview.html = getWebviewContent();
+  });
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>CSS EasyFix</title>
+</head>
+<body>
+  <h1>Welcome to CSS EasyFix!</h1>
+  <p>This is your starting point. Soon, you'll be able to inspect and edit CSS for any HTML element.</p>
+</body>
+</html>`;
+}
+
 export function deactivate() {}
